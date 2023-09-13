5cc17dec1726afc685bdac1d1fccb7177c2d3a55
Stathis <133227039+Stathis92@users.noreply.github.com>
2023-09-13 21:43:44 +0300

    Create reset button and auto adjust gridContainer size

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 document.body.onload = createGrid;
 
+const setSize = document.querySelector("button");
+
+setSize.addEventListener("click", () => {
+  customSize();
+});
+
+
 function createGrid() {
   const gridContainer = document.getElementById("gridContainer");
 
   gridContainer.style.maxHeight = "480px";
   gridContainer.style.maxWidth = "480px";
+
+  let customHeight = parseInt(gridContainer.style.maxHeight);
+  let customWidth = parseInt(gridContainer.style.maxWidth);
 
   for (let i = 0; i < GRID_SIZE; i++) {
     const gridRow = document.createElement("div");
@@ -16,9 +26,9 @@ function createGrid() {
     for (let j = 0; j < GRID_SIZE; j++) {
       let newDiv = document.createElement("div");
       newDiv.id = "gridBox" + (i * GRID_SIZE + j);
-      newDiv.style.width = "30px";
-      newDiv.style.height = "30px";
-      newDiv.style.border = "solid";
+      newDiv.style.width = (customWidth / GRID_SIZE) + "px";
+      newDiv.style.height = (customHeight / GRID_SIZE) + "px";
+      newDiv.style.border = "hidden";
       newDiv.style.borderWidth = "1px";
 
       document.getElementById(gridRow.id).appendChild(newDiv);
@@ -34,4 +44,15 @@ function mouseOver(position) {
   document.getElementById(position).style.backgroundColor = "black";
 }
 
-let GRID_SIZE = 16;
+function customSize(){
+  let input = prompt("Please enter Grid size (max 100):" );
+  GRID_SIZE = input;
+  const oldContainer = document.getElementById("gridContainer");
+  const newContainer = document.createElement("div");
+  newContainer.id = "gridContainer";
+  document.body.insertBefore(newContainer, oldContainer);
+  oldContainer.remove();
+  createGrid();
+}
+
+let GRID_SIZE = 8;
