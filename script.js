7556f32e26950067c61d57fd264c6e8c27135c5b
Stathis <133227039+Stathis92@users.noreply.github.com>
2023-09-14 14:35:40 +0300

    Add createContainer() function and custom buttons

--- script.js
+++ script.js
@@ -1,27 +1,30 @@
 document.body.onload = createGrid;
 
-const setSize = document.getElementById("reset");
-const clearSktch = document.getElementById("clear");
+const clearSketchB = document.getElementById("clear");
+const customSizeB = document.getElementById("custom");
 
-setSize.addEventListener("click", () => {
+customSizeB.addEventListener("click", () => {
   customSize();
 });
 
-clearSktch.addEventListener("click", () => {
+clearSketchB.addEventListener("click", () => {
   clearSketch();
 });
 
 function createGrid() {
   const gridContainer = document.getElementById("gridContainer");
 
+  //All container CSS
   gridContainer.style.maxHeight = "960px";
   gridContainer.style.maxWidth = "960px";
   gridContainer.style.borderStyle = "solid";
   gridContainer.style.borderWidth = "5px";
-
+  gridContainer.style.marginTop = "50px";
+  gridContainer.style.backgroundColor = "#F6F4EB";
   let customHeight = parseInt(gridContainer.style.maxHeight);
   let customWidth = parseInt(gridContainer.style.maxWidth);
 
+  //Create Rows
   for (let i = 0; i < GRID_SIZE; i++) {
     const gridRow = document.createElement("div");
     gridRow.id = "gridRow" + i;
@@ -29,46 +32,56 @@ function createGrid() {
     gridRow.style.flex = "row";
     gridContainer.appendChild(gridRow);
 
+    //Create Columns
     for (let j = 0; j < GRID_SIZE; j++) {
       let newDiv = document.createElement("div");
       newDiv.id = "gridBox" + (i * GRID_SIZE + j);
+
+      //Calculate the cell size
       newDiv.style.width = customWidth / GRID_SIZE + "px";
       newDiv.style.height = customHeight / GRID_SIZE + "px";
+
+      //Create Outline instead of borders
       newDiv.style.outline = "1px";
       newDiv.style.outlineStyle = "solid";
 
       document.getElementById(gridRow.id).appendChild(newDiv);
 
       document.getElementById(newDiv.id).onmouseover = function () {
-        mouseDown(newDiv.id);
+        mouseOver(newDiv.id);
       };
     }
   }
 }
 
-function mouseDown(position) {
+function mouseOver(position) {
   let randomColor = Math.floor(Math.random() * 16777215).toString(16);
   document.getElementById(position).style.backgroundColor = "#" + randomColor;
 }
 
 function customSize() {
   let input = prompt("Please enter Grid size (max 100):");
+  while (input > 100 || input < 1 || isNaN(input)) {
+    input = prompt("Please enter Grid size (max 100):");
+  }
   GRID_SIZE = input;
-  const oldContainer = document.getElementById("gridContainer");
-  const newContainer = document.createElement("div");
-  newContainer.id = "gridContainer";
-  document.body.insertBefore(newContainer, oldContainer);
-  oldContainer.remove();
+  createContainer();
   createGrid();
 }
 
 function clearSketch() {
+  createContainer();
+  createGrid();
+}
+
+//Function to create a new container in the same position as the last one
+//delete the old one
+function createContainer() {
   const oldContainer = document.getElementById("gridContainer");
   const newContainer = document.createElement("div");
   newContainer.id = "gridContainer";
   document.body.insertBefore(newContainer, oldContainer);
   oldContainer.remove();
-  createGrid();
 }
 
 let GRID_SIZE = 16;
